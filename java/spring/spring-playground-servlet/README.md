ddf29024aef7acac2cd95d9746eb15431a815fcf
pilhwanKim <pilhwankim03@gmail.com>
2021-03-13 17:11:26 +0900

    feat(spring-servlet): Hello 서블릿

---
+++ java/spring/spring-playground-servlet/README.md
@@ -0,0 +1,4 @@
+# 스프링 MVC : 백엔드 웹 개발 핵심 기술
+
+아래 강의 실습 프로젝트이다.
+https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-mvc-1
--- java/spring/spring-playground-servlet/src/main/java/dev/leonkim/springplaygroundservlet/SpringPlaygroundServletApplication.java
+++ java/spring/spring-playground-servlet/src/main/java/dev/leonkim/springplaygroundservlet/SpringPlaygroundServletApplication.java
@@ -2,7 +2,9 @@ package dev.leonkim.springplaygroundservlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class SpringPlaygroundServletApplication {
 
