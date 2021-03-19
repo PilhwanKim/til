783e12ba4c485fbd7873cfe0949190e68e8d6d23
Pilhwan Kim <pilhwankim03@gmail.com>
2021-03-19 19:49:45 +0900

    feat(spring-mvc-playground): 프로젝트 생성

---
+++ java/spring/spring-mvc-playground/src/main/java/dev/leonkim/springmvcplayground/SpringMvcPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package dev.leonkim.springmvcplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcPlaygroundApplication.class, args);
+    }
+
+}
---
+++ java/spring/spring-mvc-playground/src/test/java/dev/leonkim/springmvcplayground/SpringMvcPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leonkim.springmvcplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
