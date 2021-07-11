291564dca714d0d2f3882f855f4ec784ca5aa313
Pilhwan Kim <pilhwankim03@gmail.com>
2021-07-11 14:04:09 +0900

    feat(spring-mvc 타임 리프): 자바스크립트 인라인

--- java/spring/spring-mvc-playground/src/main/java/dev/leonkim/springmvcplayground/thymeleaf/BasicController.java
+++ java/spring/spring-mvc-playground/src/main/java/dev/leonkim/springmvcplayground/thymeleaf/BasicController.java
@@ -111,6 +111,13 @@ public class BasicController {
         return "thymeleaf/basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+        return "thymeleaf/basic/javascript";
+    }
+
     private void addUsers(Model model) {
         List<User> list = new ArrayList<>();
         list.add(new User("UserA", 10));
