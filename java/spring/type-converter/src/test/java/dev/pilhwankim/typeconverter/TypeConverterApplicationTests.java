c65e706c9fe41f5a54d82a95489fa2a4f6fe8c31
Pilhwan Kim <pilhwankim03@gmail.com>
2021-07-21 19:09:20 +0900

    feat(스프링 타입 컨버터): 프로젝트 생성

---
+++ java/spring/type-converter/src/test/java/dev/pilhwankim/typeconverter/TypeConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pilhwankim.typeconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
