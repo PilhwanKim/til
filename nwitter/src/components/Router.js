6599a16eedfabb1f1a19bf768475c28118e213c0
Leon Kim <pilhwankim03@gmail.com>
2021-08-29 19:55:23 +0900

    feat(리엑트-nwitter): 로그아웃

--- nwitter/src/components/Router.js
+++ nwitter/src/components/Router.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import {HashRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
 
 const AppRouter = ({isLoggedIn}) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ? (
                     <>
                         <Route exact path="/">
                             <Home />
                         </Route>
+                        <Route exact path="/profile">
+                            <Profile />
+                        </Route>
+                        <Redirect from="*" to="/" />
                     </>
                 ) : (
                     <Route exact path="/">
