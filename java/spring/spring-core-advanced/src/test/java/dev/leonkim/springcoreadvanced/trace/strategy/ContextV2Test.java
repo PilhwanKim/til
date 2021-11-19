7ca325a09cb240b0e4f22670a577a77c34ad991a
Leon Kim <pilhwankim03@gmail.com>
2021-11-20 02:12:16 +0900

    feat(spring-core-adv): 전략패턴 - 파라메터 전달 사용

---
+++ java/spring/spring-core-advanced/src/test/java/dev/leonkim/springcoreadvanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,49 @@
+package dev.leonkim.springcoreadvanced.trace.strategy;
+
+import dev.leonkim.springcoreadvanced.trace.strategy.code.strategy.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    /**
+     * 전략 패턴 사용
+     */
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스
+     */
+    @Test
+    void strategyV2() {
+        ContextV2 context = new ContextV2();
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스2, 람다 사용
+     */
+    @Test
+    void strategyV3() {
+        ContextV2 context = new ContextV2();
+        context.execute(() -> log.info("비즈니스 로직1 실행"));
+        context.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+}
