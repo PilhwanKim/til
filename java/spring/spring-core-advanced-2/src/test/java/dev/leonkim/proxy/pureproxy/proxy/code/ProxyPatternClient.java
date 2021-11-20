2529948a99a73073ad12309f25df0eca67e5f081
Leon Kim <pilhwankim03@gmail.com>
2021-11-20 19:16:27 +0900

    feat(spring-core-adv-2): 프록시 패턴 예제

---
+++ java/spring/spring-core-advanced-2/src/test/java/dev/leonkim/proxy/pureproxy/proxy/code/ProxyPatternClient.java
@@ -0,0 +1,14 @@
+package dev.leonkim.proxy.pureproxy.proxy.code;
+
+public class ProxyPatternClient {
+
+    private Subject subject;
+
+    public ProxyPatternClient(Subject subject) {
+        this.subject = subject;
+    }
+
+    public void execute() {
+        subject.operation();
+    }
+}
---
+++ java/spring/spring-core-advanced-2/src/test/java/dev/leonkim/proxy/pureproxy/proxy/code/Subject.java
@@ -0,0 +1,5 @@
+package dev.leonkim.proxy.pureproxy.proxy.code;
+
+public interface Subject {
+    String operation();
+}
