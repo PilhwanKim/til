210ecb3d0de3d4f2357510b65834bd2335fcfd61
Leon Kim <pilhwankim03@gmail.com>
2021-12-24 18:00:17 +0900

    feat(spring-microservice): 프로젝트 초기화

---
+++ micro-service/user-management/user.cmd.api/src/test/java/dev/leonkim/user/cmd/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leonkim.user.cmd.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
