90aed106682a5e257ba5f40202d6e61cfbd8a026
Leon Kim <pilhwankim03@gmail.com>
2022-03-07 17:42:36 +0900

    feat(spring-core-adv-3): 스프링 AOP 포인트컷 - this, target

---
+++ java/spring/spring-core-advanced-3/src/test/java/dev/leonkim/aop/pointcut/ThisTargetTest.java
@@ -0,0 +1,66 @@
+package dev.leonkim.aop.pointcut;
+
+import dev.leonkim.aop.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+/**
+ * (중요!!) this, target 지시자는 단독으로 사용되기 보다는 파라미터 바인딩에서 주로 사용된다.
+ *
+ * application.properties
+ * spring.aop.proxy-target-class=true CGLIB
+ * spring.aop.proxy-target-class=false JDK 동적 프록시
+ */
+@Slf4j
+@Import(ThisTargetTest.ThisTargetAspect.class)
+//@SpringBootTest(properties = "spring.aop.proxy-target-class=true") // CGLIB
+@SpringBootTest(properties = "spring.aop.proxy-target-class=false") // JDK 동적 프록시
+public class ThisTargetTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    void success() {
+        log.info("memberService Proxy={}", memberService.getClass());
+        memberService.hello("helloA");
+    }
+
+    @Slf4j
+    @Aspect
+    static class ThisTargetAspect {
+
+        // 부모 타입 허용
+        @Around("this(dev.leonkim.aop.member.MemberService)")
+        public Object doThisInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[this-interface] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        // 부모 타입 허용
+        @Around("target(dev.leonkim.aop.member.MemberService)")
+        public Object doTargetInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[target-interface] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        // (중요!!) JDK 동적 프록시(spring.aop.proxy-target-class=false) 일때 AOP 적용이 안됨
+        @Around("this(dev.leonkim.aop.member.MemberServiceImpl)")
+        public Object doThis(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[this-impl] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        @Around("target(dev.leonkim.aop.member.MemberServiceImpl)")
+        public Object doTarget(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[target-impl] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+    }
+}
