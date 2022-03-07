b9eadfb55678982a95dbeb2dad9069c2f85fd0b2
Leon Kim <pilhwankim03@gmail.com>
2022-03-07 11:59:33 +0900

    feat(spring-core-adv-3): 스프링 AOP 포인트컷 - within

---
+++ java/spring/spring-core-advanced-3/src/test/java/dev/leonkim/aop/pointcut/WithInTest.java
@@ -0,0 +1,57 @@
+package dev.leonkim.aop.pointcut;
+
+import dev.leonkim.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class WithInTest {
+
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    Method helloMethod;
+
+    @BeforeEach
+    void init() throws NoSuchMethodException {
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    @Test
+    void withinExact() {
+        pointcut.setExpression("within(dev.leonkim.aop.member.MemberServiceImpl)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void withinStar() {
+        pointcut.setExpression("within(dev.leonkim.aop.member.*Service*)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void withinSubPackage() {
+        pointcut.setExpression("within(dev.leonkim.aop..*)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    @DisplayName("타겟의 타입에만 직접 적용, 인터페이스를 선정하면 안된다.")
+    void withinSuperTypeFalse() {
+        pointcut.setExpression("within(dev.leonkim.aop.member.MemberService)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @Test
+    @DisplayName("execution은 타입 기반, 인터페이스 선정 가능")
+    void withinSuperTypeTrue() {
+        pointcut.setExpression("execution(* dev.leonkim.aop.member.MemberService.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+}
