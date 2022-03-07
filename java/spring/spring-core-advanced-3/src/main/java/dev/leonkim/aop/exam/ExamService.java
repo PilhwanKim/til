935edc234336202da44d94c82a5e4675d85ecaf4
Leon Kim <pilhwankim03@gmail.com>
2022-03-07 18:08:14 +0900

    feat(spring-core-adv-3): 스프링 AOP 실전 예제 - 로그 출력 AOP

--- java/spring/spring-core-advanced-3/src/main/java/dev/leonkim/aop/exam/ExamService.java
+++ java/spring/spring-core-advanced-3/src/main/java/dev/leonkim/aop/exam/ExamService.java
@@ -1,5 +1,6 @@
 package dev.leonkim.aop.exam;
 
+import dev.leonkim.aop.exam.annotation.Trace;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,7 @@ public class ExamService {
 
     private final ExamRepository examRepository;
 
+    @Trace
     public void request(String itemId) {
         examRepository.save(itemId);
     }
