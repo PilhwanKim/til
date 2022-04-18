3854f5cebfa209e707cae936d2117f6d70f323be
김필환[HyperCLOVA Engineering] <pilhwan.kim@navercorp.com>
2022-04-18 13:28:26 +0900

    feat(spring-db-1): JDBC 이해 - JDBC 개발 - 조회

--- java/spring/spring-db-1/src/test/java/dev/leonkim/springdb1/repository/MemberRepositoryV0Test.java
+++ java/spring/spring-db-1/src/test/java/dev/leonkim/springdb1/repository/MemberRepositoryV0Test.java
@@ -1,17 +1,30 @@
 package dev.leonkim.springdb1.repository;
 
 import dev.leonkim.springdb1.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
+        //save
         Member member = new Member("memberV0", 10000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        log.info("member == findMember {}", member == findMember);
+        log.info("member equals findMember {}", member.equals(findMember));
+        assertThat(findMember).isEqualTo(member);
     }
 }
