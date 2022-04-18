29624f0c885c90cb589f20f025f5beccd3acc38a
김필환[HyperCLOVA Engineering] <pilhwan.kim@navercorp.com>
2022-04-18 13:48:06 +0900

    feat(spring-db-1): JDBC 이해 - JDBC 개발 - 수정, 삭제

--- java/spring/spring-db-1/src/test/java/dev/leonkim/springdb1/repository/MemberRepositoryV0Test.java
+++ java/spring/spring-db-1/src/test/java/dev/leonkim/springdb1/repository/MemberRepositoryV0Test.java
@@ -2,12 +2,13 @@ package dev.leonkim.springdb1.repository;
 
 import dev.leonkim.springdb1.domain.Member;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 class MemberRepositoryV0Test {
@@ -26,5 +27,15 @@ class MemberRepositoryV0Test {
         log.info("member == findMember {}", member == findMember);
         log.info("member equals findMember {}", member.equals(findMember));
         assertThat(findMember).isEqualTo(member);
+
+        //update: money 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
