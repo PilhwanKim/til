6e5b59f6f90a04d5cb9b3d7eecf80bf46b4784de
김필환[HyperCLOVA Engineering] <pilhwan.kim@navercorp.com>
2022-04-18 10:38:15 +0900

    feat(spring-db-1): JDBC 이해 - 데이터베이스 연결

---
+++ java/spring/spring-db-1/src/main/java/dev/leonkim/springdb1/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package dev.leonkim.springdb1.connection;
+
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:mysql://localhost:3306/spring_db_1?serverTimezone=UTC&characterEncoding=UTF-8";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "!234Qwer";
+}
