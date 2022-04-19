3eebb94c785a7b77f965a84ef19b1c205850daee
김필환[HyperCLOVA Engineering] <pilhwan.kim@navercorp.com>
2022-04-19 10:01:16 +0900

    feat(spring-db-1): JDBC 이해 - DataSource 예제2 - 커넥션 풀

--- java/spring/spring-db-1/src/test/java/dev/leonkim/springdb1/connection/ConnectionTest.java
+++ java/spring/spring-db-1/src/test/java/dev/leonkim/springdb1/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package dev.leonkim.springdb1.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -29,6 +30,22 @@ public class ConnectionTest {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         // step 2) 사용 - URL, USERNAME, PASSWORD 속성들에 의존하지 않아도 됨. 보통 사용은 수많은 곳에서 함.
         useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        // 커넥션 풀이 다 찾을경우 어떻게 되는지 보려면 주석을 풀어서 실행.
+//        dataSource.setMaximumPoolSize(1);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
