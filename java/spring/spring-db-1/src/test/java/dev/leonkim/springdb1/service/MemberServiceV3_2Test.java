6d549878d2ad74cd2d19aed36f830715b149cac6
Pilhwan Kim <pilhwan.kim@navercorp.com>
2022-04-29 18:44:40 +0900

    feat(spring-db-1): 트랜잭션 이해 - 트랜잭션 문제 해결 - 트랜잭션 템플릿

--- java/spring/spring-db-1/src/test/java/dev/leonkim/springdb1/service/MemberServiceV3_2Test.java
+++ java/spring/spring-db-1/src/test/java/dev/leonkim/springdb1/service/MemberServiceV3_2Test.java
@@ -18,17 +18,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
- * 트랜잭션 - 트랜잭션 매니저
+ * 트랜잭션 - 트랜잭션 탬플릿
  */
 @Slf4j
-class MemberServiceV3_1Test {
+class MemberServiceV3_2Test {
 
     public static final String MEMBER_A = "memberA";
     public static final String MEMBER_B = "memberB";
     public static final String MEMBER_EX = "ex";
 
     private MemberRepositoryV3 memberRepository;
-    private MemberServiceV3_1 memberService;
+    private MemberServiceV3_2 memberService;
 
     @BeforeEach
     void before() {
@@ -36,7 +36,7 @@ class MemberServiceV3_1Test {
         memberRepository = new MemberRepositoryV3(dataSource);
 
         PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
-        memberService = new MemberServiceV3_1(transactionManager, memberRepository);
+        memberService = new MemberServiceV3_2(transactionManager, memberRepository);
     }
 
     @AfterEach
