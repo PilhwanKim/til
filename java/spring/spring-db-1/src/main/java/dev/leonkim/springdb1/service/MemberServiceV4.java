898e9d2fb09b9b4af9d3e8f829068d0115d2a781
Pilhwan Kim <pilhwan.kim@navercorp.com>
2022-05-02 14:50:50 +0900

    feat(spring-db-1): 스프링과 문제 해결 - 예외 처리, 반복 - 런타임 예외 적용

---
+++ java/spring/spring-db-1/src/main/java/dev/leonkim/springdb1/service/MemberServiceV4.java
@@ -0,0 +1,46 @@
+package dev.leonkim.springdb1.service;
+
+import dev.leonkim.springdb1.domain.Member;
+import dev.leonkim.springdb1.repository.MemberRepository;
+import dev.leonkim.springdb1.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+/**
+ * 예외 누수 문제 해결
+ * 체크 예외를 런타임 예외로 변경
+ *
+ * MemberRepository 인터페이스 사용
+ * throws SQLException 제거
+ */
+@Slf4j
+public class MemberServiceV4 {
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceV4(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) {
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
