028370a9fb344eecbbb531213923849a1084a89b
Pilhwan Kim <pilhwan.kim@navercorp.com>
2022-09-27 19:27:27 +0900

    feat(spring-db-2): JdbcTemplate 적용3 - 구성과 실행

---
+++ java/spring/spring-db-2/src/main/java/dev/leonkim/itemservice/config/JdbcTemplateV1Config.java
@@ -0,0 +1,31 @@
+package dev.leonkim.itemservice.config;
+
+
+import dev.leonkim.itemservice.repository.ItemRepository;
+import dev.leonkim.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV1;
+import dev.leonkim.itemservice.repository.memory.MemoryItemRepository;
+import dev.leonkim.itemservice.service.ItemService;
+import dev.leonkim.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class JdbcTemplateV1Config {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JdbcTemplateItemRepositoryV1(dataSource);
+    }
+
+}
