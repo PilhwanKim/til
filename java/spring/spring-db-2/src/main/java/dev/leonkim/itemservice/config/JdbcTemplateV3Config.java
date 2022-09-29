7eed4e33868991e776d7ad190399acc62a0c65a5
Pilhwan Kim <pilhwan.kim@navercorp.com>
2022-09-29 18:39:02 +0900

    feat(spring-db-2): JdbcTemplate - SimpleJdbcInsert

---
+++ java/spring/spring-db-2/src/main/java/dev/leonkim/itemservice/config/JdbcTemplateV3Config.java
@@ -0,0 +1,30 @@
+package dev.leonkim.itemservice.config;
+
+
+import dev.leonkim.itemservice.repository.ItemRepository;
+import dev.leonkim.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV3;
+import dev.leonkim.itemservice.service.ItemService;
+import dev.leonkim.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class JdbcTemplateV3Config {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JdbcTemplateItemRepositoryV3(dataSource);
+    }
+
+}
