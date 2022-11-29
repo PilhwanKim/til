f0158355d8b849a35f1583af5589c5b5a8c899d2
Pilhwan Kim <pilhwan.kim@navercorp.com>
2022-11-30 06:52:30 +0900

    feat(spring-db-2): 스프링 트랜잭션 이해 - 프로젝트 생성

---
+++ java/spring/spring-db-2-tx/src/main/java/dev/leonkim/springdb2tx/SpringDb2TxApplication.java
@@ -0,0 +1,13 @@
+package dev.leonkim.springdb2tx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDb2TxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDb2TxApplication.class, args);
+    }
+
+}
---
+++ java/spring/spring-db-2-tx/src/test/java/dev/leonkim/springdb2tx/SpringDb2TxApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leonkim.springdb2tx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDb2TxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
