9186a753434189de2b2543b1d26f001f9620e3db
Pilhwan Kim <pilhwan.kim@navercorp.com>
2022-11-30 19:00:05 +0900

    feat(spring-db-2): 스프링 트랜잭션 전파5 - 외부 롤백

--- java/spring/spring-db-2-tx/src/test/java/dev/leonkim/springdb2tx/propagation/BasicTxTest.java
+++ java/spring/spring-db-2-tx/src/test/java/dev/leonkim/springdb2tx/propagation/BasicTxTest.java
@@ -94,4 +94,20 @@ public class BasicTxTest {
         txManager.commit(outer);
     }
 
+    @Test
+    void outer_rollback() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 롤백");
+        txManager.rollback(outer);
+    }
+
 }
