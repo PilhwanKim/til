aa2c859b9c20fb4761486275c024205f0c3e16ea
Pilhwan Kim <pilhwan.kim@navercorp.com>
2022-12-01 15:55:28 +0900

    feat(spring-db-2): 트랜잭션 전파 활용4 - 전파 커밋

--- java/spring/spring-db-2-tx/src/test/java/dev/leonkim/springdb2tx/propagation/MemberServiceTest.java
+++ java/spring/spring-db-2-tx/src/test/java/dev/leonkim/springdb2tx/propagation/MemberServiceTest.java
@@ -61,7 +61,25 @@ class MemberServiceTest {
     @Test
     void singleTx() {
         //given
-        String username = "outerTxOff_success";
+        String username = "singleTx_success";
+
+        //when
+        memberService.joinV1(username);
+
+        //then: 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService        @Transactional:ON
+     * memberRepository     @Transactional:ON
+     * logRepository        @Transactional:ON
+     */
+    @Test
+    void outerTxOn_success() {
+        //given
+        String username = "outerTxOn_success";
 
         //when
         memberService.joinV1(username);
