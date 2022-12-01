7c0eda7b8280356de8e3a8d673d6df2574ad5730
Pilhwan Kim <pilhwan.kim@navercorp.com>
2022-12-01 15:50:24 +0900

    feat(spring-db-2): 트랜잭션 전파 활용3 - 단일 트랜잭션

--- java/spring/spring-db-2-tx/src/main/java/dev/leonkim/springdb2tx/propagation/MemberService.java
+++ java/spring/spring-db-2-tx/src/main/java/dev/leonkim/springdb2tx/propagation/MemberService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -11,6 +13,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final LogRepository logRepository;
 
+    @Transactional
     public void joinV1(String username) {
         Member member = new Member(username);
         Log logMessage = new Log(username);
