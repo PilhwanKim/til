e581b3c2fe65ac9b30aedc72c8e826f1d439d563
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-01-25 20:05:58 +0900

    feat(helloboot): 자바코드 구성 정보 사용

--- java/spring/helloboot/src/main/java/dev/leonkim/helloboot/HellobootApplication.java
+++ java/spring/helloboot/src/main/java/dev/leonkim/helloboot/HellobootApplication.java
@@ -4,23 +4,25 @@ package dev.leonkim.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-//import org.springframework.context.support.GenericApplicationContext;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.MediaType;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-
+@Configuration
 public class HellobootApplication {
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
 
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
@@ -34,31 +36,8 @@ public class HellobootApplication {
 				webServer.start();
 			}
 		};
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
+		applicationContext.register(HellobootApplication.class);
 		applicationContext.refresh();
-
-//		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-//			servletContext.addServlet("frontcontroller", new HttpServlet() {
-//				@Override
-//				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//					// 인증, 보안, 다국어, 공통 기능
-//					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-//						String name = req.getParameter("name");
-//
-//						HelloController helloController = applicationContext.getBean(HelloController.class);
-//						String ret = helloController.hello(name);
-//
-//						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-//						resp.getWriter().println(ret);
-//					} else {
-//						resp.setStatus(HttpStatus.NOT_FOUND.value());
-//					}
-//				}
-//			}).addMapping("/*");
-//
-//		});
-//		webServer.start();
 	}
 
 }
