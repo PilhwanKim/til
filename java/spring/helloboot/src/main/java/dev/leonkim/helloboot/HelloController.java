596c9a04dda8eb98942aa2ee04f0fb586fad3c0c
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-01-25 18:00:18 +0900

    feat(helloboot): Hello 컨트롤러

---
+++ java/spring/helloboot/src/main/java/dev/leonkim/helloboot/HelloController.java
@@ -0,0 +1,12 @@
+package dev.leonkim.helloboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(String name) {
+        return "Hello " + name;
+    }
+}
