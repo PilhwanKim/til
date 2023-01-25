072e13e0beb0d877e8203c5be0e33cb21a7fced3
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-01-25 21:04:22 +0900

    feat(helloboot): 테스트 코드를 이용한 테스트

---
+++ java/spring/helloboot/src/test/java/dev/leonkim/helloboot/HelloApiTest.java
@@ -0,0 +1,30 @@
+package dev.leonkim.helloboot;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloApiTest {
+    @Test
+    void helloApi() {
+        // http localhost:8080/hello?name=Spring
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res =
+                rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        // status code 200
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // header(content-type) text/plain
+        assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE))
+                .startsWith(MediaType.TEXT_PLAIN_VALUE);
+        // body Hello Spring
+        assertThat(res.getBody()).isEqualTo("Hello Spring");
+    }
+}
