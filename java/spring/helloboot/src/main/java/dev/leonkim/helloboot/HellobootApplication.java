26ff0be3db8584271cc7369c9caf0424d6ef89f2
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-01-25 18:37:17 +0900

    feat(helloboot): 서블릿 등록

--- java/spring/helloboot/src/main/java/dev/leonkim/helloboot/HellobootApplication.java
+++ java/spring/helloboot/src/main/java/dev/leonkim/helloboot/HellobootApplication.java
@@ -5,11 +5,27 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HellobootApplication {
 
 	public static void main(String[] args) {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					resp.setStatus(200);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().println("Hello Servlet");
+				}
+			}).addMapping("/hello");
+
+		});
 		webServer.start();
 	}
 
