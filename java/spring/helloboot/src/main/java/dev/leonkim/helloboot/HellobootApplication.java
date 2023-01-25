efc9b23c22efaf901f124382bb6f9e422a57818d
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-01-25 18:20:46 +0900

    feat(helloboot): Containerless 개발 준비

--- java/spring/helloboot/src/main/java/dev/leonkim/helloboot/HellobootApplication.java
+++ java/spring/helloboot/src/main/java/dev/leonkim/helloboot/HellobootApplication.java
@@ -1,13 +1,11 @@
 package dev.leonkim.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		System.out.println("Hello Containerless Standalone Application");
 	}
 
 }
