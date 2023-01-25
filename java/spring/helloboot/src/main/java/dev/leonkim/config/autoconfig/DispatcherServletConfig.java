fad31fc245725eabfa15d808391551a0f628349c
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-01-26 00:18:12 +0900

    feat(helloboot): 자동 구성 애노테이션 적용

--- java/spring/helloboot/src/main/java/dev/leonkim/config/autoconfig/DispatcherServletConfig.java
+++ java/spring/helloboot/src/main/java/dev/leonkim/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package dev.leonkim.config.autoconfig;
 
+import dev.leonkim.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
     @Bean
     public DispatcherServlet dispatcherServlet() {
--- java/spring/helloboot/src/main/java/dev/leonkim/config/autoconfig/TomcatWebServerConfig.java
+++ java/spring/helloboot/src/main/java/dev/leonkim/config/autoconfig/TomcatWebServerConfig.java
@@ -1,11 +1,11 @@
 package dev.leonkim.config.autoconfig;
 
+import dev.leonkim.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@MyAutoConfiguration
 public class TomcatWebServerConfig {
     @Bean
     public ServletWebServerFactory servletWebServerFactory() {
