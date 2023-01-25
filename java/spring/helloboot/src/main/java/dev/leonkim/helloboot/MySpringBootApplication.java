6d1ff92a8afa5219a842ef870b9ee215cb7a98c9
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-01-25 23:26:32 +0900

    feat(helloboot): 합성 애노테이션의 적용

---
+++ java/spring/helloboot/src/main/java/dev/leonkim/helloboot/MySpringBootApplication.java
@@ -0,0 +1,16 @@
+package dev.leonkim.helloboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Configuration
+@ComponentScan
+public @interface MySpringBootApplication {
+}
