b400d4c07adadd270d12b4564cc330e534ede809
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-01-25 20:25:13 +0900

    feat(helloboot): Bean의 생명주기 메소드

--- java/spring/helloboot/src/main/java/dev/leonkim/helloboot/HelloController.java
+++ java/spring/helloboot/src/main/java/dev/leonkim/helloboot/HelloController.java
@@ -1,5 +1,6 @@
 package dev.leonkim.helloboot;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,12 @@ import java.util.Objects;
 @RestController
 public class HelloController {
     final private HelloService helloService;
+    private ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
+        System.out.println(applicationContext);
     }
 
     @GetMapping("/hello")
--- java/spring/helloboot/src/main/java/dev/leonkim/helloboot/HellobootApplication.java
+++ java/spring/helloboot/src/main/java/dev/leonkim/helloboot/HellobootApplication.java
@@ -4,6 +4,7 @@ package dev.leonkim.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -12,6 +13,15 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class HellobootApplication {
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
+
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
 
 	public static void main(String[] args) {
 		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
@@ -19,11 +29,12 @@ public class HellobootApplication {
 			protected void onRefresh() {
 				super.onRefresh();
 
-				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet",
-							new DispatcherServlet(this)
-					).addMapping("/*");
+					servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+							.addMapping("/*");
 				});
 				webServer.start();
 			}
