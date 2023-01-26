dd3b6e540fde46e568aef2a0a41a5fc13a266765
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-01-26 18:26:22 +0900

    feat(helloboot): 프로퍼티 클래스의 분리

--- java/spring/helloboot/src/main/java/dev/leonkim/config/autoconfig/TomcatWebServerConfig.java
+++ java/spring/helloboot/src/main/java/dev/leonkim/config/autoconfig/TomcatWebServerConfig.java
@@ -2,24 +2,22 @@ package dev.leonkim.config.autoconfig;
 
 import dev.leonkim.config.ConditionalMyOnClass;
 import dev.leonkim.config.MyAutoConfiguration;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
-    @Value("${contextPath}")
-    String contextPath;
-
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory() {
+    public ServletWebServerFactory servletWebServerFactory(ServerProperties properties) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(this.contextPath);
+
+        factory.setContextPath(properties.getContextPath());
+        factory.setPort(properties.getPort());
+
         return factory;
     }
 }
