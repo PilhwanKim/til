e81aa2608e8107e99be722ad46e69c195cdab516
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-01-26 18:02:34 +0900

    feat(helloboot): 자동 구성에 Environment 프로퍼티 적용

--- java/spring/helloboot/src/test/java/dev/leonkim/helloboot/HelloApiTest.java
+++ java/spring/helloboot/src/test/java/dev/leonkim/helloboot/HelloApiTest.java
@@ -17,7 +17,7 @@ public class HelloApiTest {
         TestRestTemplate rest = new TestRestTemplate();
 
         ResponseEntity<String> res =
-                rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+                rest.getForEntity("http://localhost:8080/app/hello?name={name}", String.class, "Spring");
 
         // status code 200
         assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
@@ -33,7 +33,7 @@ public class HelloApiTest {
         TestRestTemplate rest = new TestRestTemplate();
 
         ResponseEntity<String> res =
-                rest.getForEntity("http://localhost:8080/hello?name=", String.class);
+                rest.getForEntity("http://localhost:8080/app/hello?name=", String.class);
 
         assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
     }
