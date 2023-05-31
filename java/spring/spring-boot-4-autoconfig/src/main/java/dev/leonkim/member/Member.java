157da064929f4cce26a570c9d25dae795d1f8349
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-05-31 09:10:52 +0900

    feat(spring-boot-4): 예제 만들기

---
+++ java/spring/spring-boot-4-autoconfig/src/main/java/dev/leonkim/member/Member.java
@@ -0,0 +1,17 @@
+package dev.leonkim.member;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private String memberId;
+    private String name;
+
+    public Member() {
+    }
+
+    public Member(String memberId, String name) {
+        this.memberId = memberId;
+        this.name = name;
+    }
+}
---
+++ java/spring/spring-boot-4-autoconfig/src/test/java/dev/leonkim/member/MemberRepositoryTest.java
@@ -0,0 +1,25 @@
+package dev.leonkim.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@SpringBootTest
+public class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Transactional
+    @Test
+    void memberTest() {
+        Member member = new Member("idA", "memberA");
+        memberRepository.initTable();
+        memberRepository.save(member);
+        Member findMember = memberRepository.find(member.getMemberId());
+        Assertions.assertThat(findMember.getMemberId()).isEqualTo(member.getMemberId());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
+    }
+}
