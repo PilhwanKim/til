30961c1b32e413d615c4af46e9b0a9782eda0240
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-06-05 15:30:54 +0900

    feat(spring-boot-5): 순수 라이브러리 만들기

---
+++ java/spring/spring-boot-5-memory/src/test/java/dev/leonkim/memory/MemoryFinderTest.java
@@ -0,0 +1,14 @@
+package dev.leonkim.memory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemoryFinderTest {
+    @Test
+    void get() {
+        MemoryFinder memoryFinder = new MemoryFinder();
+        Memory memory = memoryFinder.get();
+        assertThat(memory).isNotNull();
+    }
+}
