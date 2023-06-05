b48f48b768aa6a183a2bb2a84afff47a665f5308
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-06-05 11:06:25 +0900

    feat(spring-boot-4): @Conditional - 다양한 기능

--- java/spring/spring-boot-4-autoconfig/src/main/java/dev/leonkim/hello/config/MemoryConfig.java
+++ java/spring/spring-boot-4-autoconfig/src/main/java/dev/leonkim/hello/config/MemoryConfig.java
@@ -1,14 +1,16 @@
 package dev.leonkim.hello.config;
 
-import dev.leonkim.memory.MemoryCondition;
+//import dev.leonkim.memory.MemoryCondition;
 import dev.leonkim.memory.MemoryController;
 import dev.leonkim.memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
+//import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Conditional(MemoryCondition.class)
+//@Conditional(MemoryCondition.class)
+@ConditionalOnProperty(name = "memory", havingValue = "on")
 public class MemoryConfig {
 
     @Bean
