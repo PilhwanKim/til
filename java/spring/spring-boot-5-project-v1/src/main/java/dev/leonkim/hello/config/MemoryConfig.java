87cf1d4644b7d541a0a17154c233b1eacba504ba
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-06-05 19:35:14 +0900

    feat(spring-boot-5): 순수 라이브러리 사용하기

---
+++ java/spring/spring-boot-5-project-v1/src/main/java/dev/leonkim/hello/config/MemoryConfig.java
@@ -0,0 +1,20 @@
+package dev.leonkim.hello.config;
+
+import dev.leonkim.memory.MemoryController;
+import dev.leonkim.memory.MemoryFinder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MemoryConfig {
+
+    @Bean
+    public MemoryFinder memoryFinder() {
+        return new MemoryFinder();
+    }
+
+    @Bean
+    public MemoryController memoryController() {
+        return new MemoryController(memoryFinder());
+    }
+}
