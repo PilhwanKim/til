84ee97d26e65befeef1d646e8b57831b47d133d9
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-06-21 14:35:34 +0900

    feat(spring-boot-6): 외부설정과 프로필 1 프로젝트 설정

---
+++ java/spring/spring-boot-6-external/src/main/java/dev/leonkim/ExternalApplication.java
@@ -0,0 +1,13 @@
+package dev.leonkim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalApplication.class, args);
+    }
+
+}
---
+++ java/spring/spring-boot-6-external/src/test/java/dev/leonkim/ExternalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leonkim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
