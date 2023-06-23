57e8be7f49e7d38178dd038bb64b6195b6564b11
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-06-23 16:50:17 +0900

    feat(spring-boot-7): 외부 설정 사용 - Environment

--- java/spring/spring-boot-7-external-read/src/main/java/dev/leonkim/ExternalReadApplication.java
+++ java/spring/spring-boot-7-external-read/src/main/java/dev/leonkim/ExternalReadApplication.java
@@ -1,9 +1,12 @@
 package dev.leonkim;
 
+import dev.leonkim.config.MyDataSourceEnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(MyDataSourceEnvConfig.class)
+@SpringBootApplication(scanBasePackages = "dev.leonkim.datasource")
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
