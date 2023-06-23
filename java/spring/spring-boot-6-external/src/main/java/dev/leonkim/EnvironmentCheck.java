c8bf2d0e0e39735d1ec58127239d6c6f8b48e294
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-06-23 14:49:23 +0900

    feat(spring-boot-6): 우선순위 - 설정 데이터

--- java/spring/spring-boot-6-external/src/main/java/dev/leonkim/EnvironmentCheck.java
+++ java/spring/spring-boot-6-external/src/main/java/dev/leonkim/EnvironmentCheck.java
@@ -20,10 +20,8 @@ public class EnvironmentCheck {
         String url = env.getProperty("url");
         String username = env.getProperty("username");
         String password = env.getProperty("password");
-        String common = env.getProperty("common");
         log.info("env url={}", url);
         log.info("env username={}", username);
         log.info("env password={}", password);
-        log.info("env common={}", common);
     }
 }
