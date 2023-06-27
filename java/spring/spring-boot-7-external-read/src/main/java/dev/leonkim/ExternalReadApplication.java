cc8918ce285fbd50ddec82d78377a40626e6a5d1
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-06-27 22:39:49 +0900

    feat(spring-boot-7): 외부설정 사용 - @ConfigurationProperties 시작

--- java/spring/spring-boot-7-external-read/src/main/java/dev/leonkim/ExternalReadApplication.java
+++ java/spring/spring-boot-7-external-read/src/main/java/dev/leonkim/ExternalReadApplication.java
@@ -1,14 +1,18 @@
 package dev.leonkim;
 
+import dev.leonkim.config.MyDataSourceConfigV1;
 import dev.leonkim.config.MyDataSourceEnvConfig;
 import dev.leonkim.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
 //@Import(MyDataSourceEnvConfig.class)
-@Import(MyDataSourceValueConfig.class)
+//@Import(MyDataSourceValueConfig.class)
+@Import(MyDataSourceConfigV1.class)
 @SpringBootApplication(scanBasePackages = "dev.leonkim.datasource")
+@ConfigurationPropertiesScan({"dev.leonkim"})
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
---
+++ java/spring/spring-boot-7-external-read/src/main/java/dev/leonkim/config/MyDataSourceConfigV1.java
@@ -0,0 +1,29 @@
+package dev.leonkim.config;
+
+import dev.leonkim.datasource.MyDataSource;
+import dev.leonkim.datasource.MyDataSourcePropertiesV1;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@Slf4j
+//@EnableConfigurationProperties(MyDataSourcePropertiesV1.class)
+public class MyDataSourceConfigV1 {
+    private final MyDataSourcePropertiesV1 properties;
+
+    public MyDataSourceConfigV1(MyDataSourcePropertiesV1 properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    public MyDataSource dataSource() {
+        return new MyDataSource(
+                properties.getUrl(),
+                properties.getUsername(),
+                properties.getPassword(),
+                properties.getEtc().getMaxConnection(),
+                properties.getEtc().getTimeout(),
+                properties.getEtc().getOptions()
+        );
+    }
+}
