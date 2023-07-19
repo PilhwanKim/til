a470b832a4831c0301052bd54e3a4f7ec884fb92
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-07-20 08:45:39 +0900

    feat(spring-boot-8): 메트릭 등록1 - 카운터

---
+++ java/spring/spring-boot-8-actuator/src/main/java/dev/leonkim/order/v1/OrderConfigV1.java
@@ -0,0 +1,15 @@
+package dev.leonkim.order.v1;
+
+import dev.leonkim.order.OrderService;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrderConfigV1 {
+
+    @Bean
+    OrderService orderService(MeterRegistry registry) {
+        return new OrderServiceV1(registry);
+    }
+}
