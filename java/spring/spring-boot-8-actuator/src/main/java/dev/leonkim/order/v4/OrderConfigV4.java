56375241dc459ef01e9a44794240a469a7e2c87f
Pilhwan Kim <pilhwan.kim@navercorp.com>
2023-07-20 09:38:22 +0900

    feat(spring-boot-8): 메트릭 등록4 - @Timed

---
+++ java/spring/spring-boot-8-actuator/src/main/java/dev/leonkim/order/v4/OrderConfigV4.java
@@ -0,0 +1,21 @@
+package dev.leonkim.order.v4;
+
+import dev.leonkim.order.OrderService;
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrderConfigV4 {
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceV4();
+    }
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
+}
