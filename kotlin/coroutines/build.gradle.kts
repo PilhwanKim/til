7c425b7da08c0ff945823e6103dcd3b9ea51b26d
Pilhwan Kim <pilhwankim03@gmail.com>
2024-10-23 00:11:24 +0900

    feat(kt-coroutines): 섹션2-1. 스레드 기반 작업의 한계와 코루틴의 등장

--- kotlin/coroutines/build.gradle.kts
+++ kotlin/coroutines/build.gradle.kts
@@ -10,8 +10,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    testImplementation(kotlin("test"))
+    // 코루틴 라이브러리
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+//    testImplementation(kotlin("test"))
 }
 
 tasks.test {
